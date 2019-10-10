4d315be3bc54dda7e86b85b177bf22a16fd55f92
Santosh Nagaral <santosh.nagaral@sonder.com>
2019-10-10 09:54:05 -0700

    Updating copy location

--- install_spacemacs.sh
+++ install_spacemacs.sh
@@ -1,7 +1,6 @@
 sudo add-apt-repository -y ppa:kelleyk/emacs
 sudo apt update
 sudo apt install -y emacs26
-cd ~
-mv .emacs.d .emacs.d.bak
-mv .emacs .emacs.bak
+mv ~/.emacs.d .~/emacs.d.bak
+mv ~/.emacs ~/.emacs.bak
 cp  .spacemacs ~/.emacs.d
