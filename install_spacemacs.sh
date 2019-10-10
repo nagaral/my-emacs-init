8ee7f68e21a5eef37bbf2f7edafc9cec41b4d62d
Santosh Nagaral <santosh.nagaral@sonder.com>
2019-10-10 09:52:23 -0700

    updating copy command

--- install_spacemacs.sh
+++ install_spacemacs.sh
@@ -4,4 +4,4 @@ sudo apt install -y emacs26
 cd ~
 mv .emacs.d .emacs.d.bak
 mv .emacs .emacs.bak
-git clone https://github.com/nagaral/my-emacs-init.git/.spacemacs ~/.emacs.d
+cp  .spacemacs ~/.emacs.d
